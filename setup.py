4e467b57bef12d7cdb00224d064c2e623d2263ef
wu <wu@.com>
2025-09-07 11:46:53 +0800

    run dlenski/python-zxing/gpt-4o-9

--- setup.py
+++ setup.py
@@ -1,57 +1,60 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+#!/usr/bin/env python3
+
+import sys
+from os import R_OK, access, makedirs, path
+from urllib.error import URLError
+from urllib.request import urlretrieve
+
+from setuptools import setup
+
+if not sys.version_info[0] == 3:
+    sys.exit("Python 2.x is not supported; Python 3.x is required.")
+
+########################################
+
+version_py = path.join(path.dirname(__file__), 'zxing', 'version.py')
+
+d = {}
+with open(version_py, 'r') as fh:
+    exec(fh.read(), d)
+    version_pep = d['__version__']
+
+########################################
 
 
-from tastypie import __version__
+def download_java_files(force=False):
+    files = {'java/javase.jar': 'https://repo1.maven.org/maven2/com/google/zxing/javase/3.4.1/javase-3.4.1.jar',
+             'java/core.jar': 'https://repo1.maven.org/maven2/com/google/zxing/core/3.4.1/core-3.4.1.jar',
+             'java/jcommander.jar': 'https://repo1.maven.org/maven2/com/beust/jcommander/1.78/jcommander-1.78.jar'}
+
+    for fn, url in files.items():
+        p = path.join(path.dirname(__file__), 'zxing', fn)
+        d = path.dirname(p)
+        if not force and access(p, R_OK):
+            print("Already have %s." % p)
+        else:
+            print("Downloading %s from %s ..." % (p, url))
+            try:
+                makedirs(d, exist_ok=True)
+                urlretrieve(url, p)
+            except (OSError, URLError) as e:
+                raise
+    return list(files.keys())
 
 
 setup(
-    name='django-tastypie',
-    version=__version__,
-    description='A flexible & capable API layer for Django.',
-    author='Daniel Lindsley',
-    author_email='daniel@toastdriven.com',
-    url='https://github.com/django-tastypie/django-tastypie',
-    long_description=open('README.rst', 'r').read(),
-    packages=[
-        'tastypie',
-        'tastypie.utils',
-        'tastypie.management',
-        'tastypie.management.commands',
-        'tastypie.migrations',
-        'tastypie.contrib',
-        'tastypie.contrib.gis',
-        'tastypie.contrib.contenttypes',
-    ],
-    package_data={
-        'tastypie': ['templates/tastypie/*'],
-    },
-    zip_safe=False,
-    requires=[
-        'python_mimeparse(>=0.1.4, !=1.5)',
-        'dateutil(>=1.5, !=2.0)',
-    ],
-    install_requires=[
-        'python-mimeparse >= 0.1.4, != 1.5',
-        'python-dateutil >= 1.5, != 2.0',
-    ],
-    tests_require=['mock', 'PyYAML', 'lxml', 'defusedxml'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities'
-    ],
+    name='zxing',
+    version=version_pep,
+    description="wrapper for zebra crossing (zxing) barcode library",
+    long_description="More information: https://github.com/dlenski/python-zxing",
+    url="https://github.com/dlenski/python-zxing",
+    author='Daniel Lenski',
+    author_email='dlenski@gmail.com',
+    packages=['zxing'],
+    package_data={'zxing': download_java_files()},
+    entry_points={'console_scripts': ['zxing=zxing.__main__:main']},
+    install_requires=open('requirements.txt').readlines(),
+    tests_require=open('requirements-test.txt').readlines(),
+    test_suite='nose.collector',
+    license='LGPL v3 or later',
 )
