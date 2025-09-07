4540e2fc9c915328cf6a28aafac877b47bd32221
wu <wu@.com>
2025-09-07 11:45:21 +0800

    run cloud-bulldozer/cerberus/gpt-4o-9

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from pkg_resources import VersionConflict, require
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
-requires = []
-with open('requirements.txt') as f:
-    for line in f.readlines():
-        line = line.strip()  # Remove spaces
-        line = line.split('#')[0]  # Remove comments
-        if line:  # Remove empty lines
-            requires.append(line)
 
-setup(
-    name='django-clickhouse',
-    version='1.0.3',
-    packages=['django_clickhouse'],
-    package_dir={'': 'src'},
-    url='https://github.com/carrotquest/django-clickhouse',
-    license='BSD 3-clause "New" or "Revised" License',
-    author='Carrot quest',
-    author_email='m1ha@carrotquest.io',
-    description='Django extension to integrate with ClickHouse database',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requires
-)
+if __name__ == "__main__":
+    setup()
