b10e8487ec6eb9b41077b886edb23a149b4af480
wu <wu@.com>
2025-09-07 11:57:43 +0800

    run roskakori/pygount/gpt-4o-9

--- setup.py
+++ setup.py
@@ -1,29 +1,58 @@
+"""
+Setup for pygount.
+
+Developer cheat sheet
+---------------------
+
+Tag a release (simply replace ``1.x.x`` with the current version number)::
+
+  $ git tag -a -m "Tagged version 1.x.x" v1.x.x
+  $ git push --tags
+
+Upload release to PyPI::
+
+  $ python3 setup.py bdist_wheel
+  $ twine upload dist/*.whl
+"""
+# Copyright (c) 2016-2020, Thomas Aglassinger.
+# All rights reserved. Distributed under the BSD License.
 import os
 from setuptools import setup, find_packages
 
-from tos import VERSION
+from pygount import __version__
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-readme = f.read()
-f.close()
+
+# Read the long description from the README file.
+_setup_folder = os.path.dirname(__file__)
+with open(os.path.join(_setup_folder, "README.md"), encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 setup(
-    name='django-tos',
-    version=".".join(map(str, VERSION)),
-    description='django-tos is a reusable Django application for setting Terms of Service.',
-    long_description=readme,
-    author='Frank Wiles',
-    author_email='frank@revsys.com',
-    url='http://github.com/revsys/django-tos/tree/master',
-    packages=find_packages(),
-    include_package_data=True,
+    name="pygount",
+    version=__version__,
+    description="count source lines of code (SLOC) using pygments",
+    long_description=long_description,
+    url="https://github.com/roskakori/pygount",
+    author="Thomas Aglassinger",
+    author_email="roskakori@users.sourceforge.net",
+    license="BSD",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
     ],
+    keywords=["code analysis", "count", "SLOC"],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["pygments>=2.0"],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["pygount=pygount.command:main"]},
 )
